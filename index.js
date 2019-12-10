f7fa8dadc5e8ff0b7865c6b959560be00e5155e7
Adam Dawkins <adam@dragondrop.uk>
2019-12-10 21:06:43 +0000

    Add category flow

--- index.js
+++ index.js
@@ -10,20 +10,16 @@ const contains = curry((list, item) => list.indexOf(item) > -1);
 //    collection :: {k: v} -> [{k:v}]
 const collection = obj =>
   Object.entries(obj).map(([id, props]) => ({ ...props, id }));
+
 // HELPERS
 
 //    getStatus :: (State, String) -> String
-const getStatus = (state, optionId) => {
-  if (isSelected(state, optionId)) {
-    return "SELECTED";
-  }
-
-  if (isDisabled(state, optionId)) {
-    return "DISABLED";
-  }
-
-  return "AVAILABLE";
-};
+const getStatus = (state, optionId) =>
+  isSelected(state, optionId)
+    ? "SELECTED"
+    : isDisabled(state, optionId)
+    ? "DISABLED"
+    : "AVAILABLE";
 
 //    isSelected :: State -> String -> Boolean
 const isSelected = curry((state, optionId) =>
@@ -81,8 +77,12 @@ const isDisabled = (state, optionId) => {
   return disabled;
 };
 
-//    createOption :: (String, String) -> Object
-const createOption = (name, ruleId) => ({ name, rules: [ruleId] });
+//    createOption :: (String, String, category) -> Object
+const createOption = (name, ruleId, category) => ({
+  name,
+  category,
+  rules: [ruleId]
+});
 
 //    createRule :: (String, String) -> Object
 const createRule = (ruleId, type) => ({
@@ -100,18 +100,24 @@ const addOptionToRule = (optionId, isPrimary, rule) => ({
     : rule.childOptions.concat([optionId])
 });
 
-//    findOrCreateOption :: (State, String, String, String) -> Option
-const findOrCreateOption = (state, optionId, name, ruleId) =>
-  state.options[optionId] || createOption(name, ruleId);
+//    findOrCreateOption :: (State, String, String, String, category) -> Option
+const findOrCreateOption = (state, optionId, name, ruleId, category) =>
+  state.options[optionId] || createOption(name, ruleId, category);
 
 //    findOrCreateRule :: (State, String, String) -> Rule
 const findOrCreateRule = (state, ruleId, type) =>
   state.rules[ruleId] || createRule(ruleId, type);
 
-//    addOption :: [string, string, string, int, string] -> State -> State
+//    addOption :: [string, string, string, int, string, string] -> State -> State
 const addOption = curry((option, state) => {
-  const [ruleId, type, optionId, primary, name] = option;
-  const updatedOption = findOrCreateOption(state, optionId, name, ruleId);
+  const [ruleId, type, optionId, primary, name, category] = option;
+  const updatedOption = findOrCreateOption(
+    state,
+    optionId,
+    name,
+    ruleId,
+    category
+  );
   updatedOption.rules.push(ruleId);
 
   const updatedRule = addOptionToRule(
@@ -146,154 +152,380 @@ const print = state => {
 
 // ACTIONS
 
-const SelectOption = (state, optionId) => {
+const NextCategory = state => {
+  const newPosition = state.categories.hasNext
+    ? state.categories.position + 1
+    : state.categories.position;
+
   return {
     ...state,
-    selected: state.selected.concat([optionId]),
-    activeRules: state.activeRules.concat(state.options[optionId].rules)
+    categories: {
+      ...state.categories,
+      position: newPosition,
+      current: state.categories.values[newPosition],
+      hasPrevious: true,
+      hasNext: newPosition < state.categories.size - 1
+    }
   };
 };
 
-const ToggleOption = (state, optionId) => {
-  return contains(state.selected, optionId)
+const SelectOption = (state, optionId) => ({
+  ...state,
+  selected: state.selected.concat([optionId]),
+  activeRules: state.activeRules.concat(state.options[optionId].rules)
+});
+
+const ToggleOption = (state, optionId) =>
+  contains(state.selected, optionId)
     ? DeSelectOption(state, optionId)
     : SelectOption(state, optionId);
-};
 
-const DeSelectOption = (state, optionId) => {
-  return {
-    ...state,
-    selected: state.selected.filter(id => id !== optionId),
-    activeRules: state.activeRules.filter(
-      ruleId =>
-        !(
-          contains(state.options[optionId].rules, ruleId) &&
-          numberOfSelectedOptionsForRule(state, state.rules[ruleId]) === 1
-        )
-    )
-  };
-};
+const DeSelectOption = (state, optionId) => ({
+  ...state,
+  selected: state.selected.filter(id => id !== optionId),
+  activeRules: state.activeRules.filter(
+    ruleId =>
+      !(
+        contains(state.options[optionId].rules, ruleId) &&
+        numberOfSelectedOptionsForRule(state, state.rules[ruleId]) === 1
+      )
+  )
+});
 
-const getInitialState = () =>
-  pipe(
-    addOption(["7060187", "OO", "20844", 0, "Pearl - Arrow Grey"]),
-    addOption(["7060187", "OO", "27128", 0, "Pearl - Misano Red"]),
-    addOption(["7060187", "OO", "93020", 0, "Metallic - Glacier white"]),
-    addOption(["7060187", "OO", "104177", 0, "Metallic - Mythos black"]),
-    addOption(["7060187", "OO", "105356", 0, "Solid - Shell white"]),
-    addOption(["7060187", "OO", "111413", 0, "Metallic - Manhattan grey"]),
-    addOption(["7060187", "OO", "122473", 0, "Metallic - Firmament blue"]),
-    addOption(["7060187", "OO", "125093", 0, "Metallic - Python yellow"]),
-    addOption(["7060187", "OO", "129827", 0, "Special solid - Tioman green"]),
-    addOption(["7060188", "OO", "97650", 0, "16 10 Spoke design alloy wheels"]),
+const getInitialState = () => {
+  return pipe(
+    addOption(["7060187", "OO", "20844", 0, "Pearl - Arrow Grey", "colour"]),
+    addOption(["7060187", "OO", "27128", 0, "Pearl - Misano Red", "colour"]),
+    addOption([
+      "7060187",
+      "OO",
+      "93020",
+      0,
+      "Metallic - Glacier white",
+      "colour"
+    ]),
+    addOption([
+      "7060187",
+      "OO",
+      "104177",
+      0,
+      "Metallic - Mythos black",
+      "colour"
+    ]),
+    addOption(["7060187", "OO", "105356", 0, "Solid - Shell white", "colour"]),
+    addOption([
+      "7060187",
+      "OO",
+      "111413",
+      0,
+      "Metallic - Manhattan grey",
+      "colour"
+    ]),
+    addOption([
+      "7060187",
+      "OO",
+      "122473",
+      0,
+      "Metallic - Firmament blue",
+      "colour"
+    ]),
+    addOption([
+      "7060187",
+      "OO",
+      "125093",
+      0,
+      "Metallic - Python yellow",
+      "colour"
+    ]),
+    addOption([
+      "7060187",
+      "OO",
+      "129827",
+      0,
+      "Special solid - Tioman green",
+      "colour"
+    ]),
+    addOption([
+      "7060188",
+      "OO",
+      "97650",
+      0,
+      "16 10 Spoke design alloy wheels",
+      "wheels"
+    ]),
     addOption([
       "7060188",
       "OO",
       "111416",
       0,
-      "17 5 arm star design alloy wheels"
+      "17 5 arm star design alloy wheels",
+      "wheels"
     ]),
     addOption([
       "7060188",
       "OO",
       "125055",
       "0",
-      "16 10 spoke turbine design alloy wheels"
+      "16 10 spoke turbine design alloy wheels",
+      "wheels"
     ]),
     addOption([
       "7060189",
       "OO",
       "78562",
       "0",
-      "3 spoke flat bottomed multi-function leather steering wheel"
+      "3 spoke flat bottomed multi-function leather steering wheel",
+      "other"
     ]),
     addOption([
       "7060189",
       "OO",
       "129818",
       "0",
-      "3 spoke leather multifunction plus steering wheel"
+      "3 spoke leather multifunction plus steering wheel",
+      "other"
     ]),
-    addOption(["7060191", "OO", "125084", 0, "Contrast roof - Manhattan grey"]),
-    addOption(["7060191", "OO", "125085", 0, "Contrast roof - Mythos black"]),
-    addOption(["7060203", "RO", "20844", 0, "Pearl - Arrow Grey"]),
-    addOption(["7060203", "RO", "27128", 0, "Pearl - Misano Red"]),
-    addOption(["7060203", "RO", "93020", 0, "Metallic - Glacier white"]),
-    addOption(["7060203", "RO", "104177", 0, "Metallic - Mythos black"]),
-    addOption(["7060203", "RO", "122473", 0, "Metallic - Firmament blue"]),
-    addOption(["7060203", "RO", "125084", 1, "Contrast roof - Manhattan grey"]),
-    addOption(["7060203", "RO", "125093", 0, "Metallic - Python yellow"]),
-    addOption(["7060203", "RO", "129827", 0, "Special solid - Tioman green"]),
-    addOption(["7060204", "RO", "20844", 0, "Pearl - Arrow Grey"]),
-    addOption(["7060204", "RO", "27128", 0, "Pearl - Misano Red"]),
-    addOption(["7060204", "RO", "93020", 0, "Metallic - Glacier white"]),
-    addOption(["7060204", "RO", "111413", 0, "Metallic - Manhattan grey"]),
-    addOption(["7060204", "RO", "122473", 0, "Metallic - Firmament blue"]),
-    addOption(["7060204", "RO", "125085", 1, "Contrast roof - Mythos black"]),
-    addOption(["7060204", "RO", "125093", 0, "Metallic - Python yellow"]),
-    addOption(["7060204", "RO", "129827", 0, "Special solid - Tioman green"]),
+    addOption([
+      "7060191",
+      "OO",
+      "125084",
+      0,
+      "Contrast roof - Manhattan grey",
+      "exterior body features"
+    ]),
+    addOption([
+      "7060191",
+      "OO",
+      "125085",
+      0,
+      "Contrast roof - Mythos black",
+      "exterior body features"
+    ]),
+    addOption(["7060203", "RO", "20844", 0, "Pearl - Arrow Grey", "colour"]),
+    addOption(["7060203", "RO", "27128", 0, "Pearl - Misano Red", "colour"]),
+    addOption([
+      "7060203",
+      "RO",
+      "93020",
+      0,
+      "Metallic - Glacier white",
+      "colour"
+    ]),
+    addOption([
+      "7060203",
+      "RO",
+      "104177",
+      0,
+      "Metallic - Mythos black",
+      "colour"
+    ]),
+    addOption([
+      "7060203",
+      "RO",
+      "122473",
+      0,
+      "Metallic - Firmament blue",
+      "colour"
+    ]),
+    addOption([
+      "7060203",
+      "RO",
+      "125084",
+      1,
+      "Contrast roof - Manhattan grey",
+      "exterior body features"
+    ]),
+    addOption([
+      "7060203",
+      "RO",
+      "125093",
+      0,
+      "Metallic - Python yellow",
+      "colour"
+    ]),
+    addOption([
+      "7060203",
+      "RO",
+      "129827",
+      0,
+      "Special solid - Tioman green",
+      "colour"
+    ]),
+    addOption(["7060204", "RO", "20844", 0, "Pearl - Arrow Grey", "colour"]),
+    addOption(["7060204", "RO", "27128", 0, "Pearl - Misano Red", "colour"]),
+    addOption([
+      "7060204",
+      "RO",
+      "93020",
+      0,
+      "Metallic - Glacier white",
+      "colour"
+    ]),
+    addOption([
+      "7060204",
+      "RO",
+      "111413",
+      0,
+      "Metallic - Manhattan grey",
+      "colour"
+    ]),
+    addOption([
+      "7060204",
+      "RO",
+      "122473",
+      0,
+      "Metallic - Firmament blue",
+      "colour"
+    ]),
+    addOption([
+      "7060204",
+      "RO",
+      "125085",
+      1,
+      "Contrast roof - Mythos black",
+      "exterior body features"
+    ]),
+    addOption([
+      "7060204",
+      "RO",
+      "125093",
+      0,
+      "Metallic - Python yellow",
+      "colour"
+    ]),
+    addOption([
+      "7060204",
+      "RO",
+      "129827",
+      0,
+      "Special solid - Tioman green",
+      "colour"
+    ]),
     addOption([
       "7060205",
       "OO",
       "135657",
       0,
-      "Novum cloth - Granite grey with mint contrast + front sport seats"
+      "Novum cloth - Granite grey with mint contrast + front sport seats",
+      "trim"
     ]),
     addOption([
       "7060205",
       "OO",
       "143138",
       0,
-      "Novum cloth - Granite grey  steel grey contrast stitching + front sport seats"
+      "Novum cloth - Granite grey  steel grey contrast stitching + front sport seats",
+      "trim"
     ]),
-    addOption(["7062150", "OO", "1753", 0, "Manual air conditioning"]),
-    addOption(["7062150", "OO", "139671", 0, "Plus pack - A1 Sportback"]),
-    addOption(["7062150", "OO", "139671", 0, "Plus pack - A1 Sportback"]),
-    addOption(["7062150", "OO", "139671", 0, "Plus pack - A1 Sportback"]),
-    addOption(["7062151", "RA", "140070", 1, "Space saving spare wheel"]),
-    addOption(["7062151", "RA", "140071", 0, "Tool kit and Jack"]),
-    addOption(["7062153", "OO", "69372", 0, "Tyre repair kit"]),
-    addOption(["7062153", "OO", "140070", 0, "Space saving spare wheel"]),
-    addOption(["7062154", "OO", "2427", 0, "Tool kit"]),
-    addOption(["7062154", "OO", "140071", 0, "Tool kit and Jack"]),
+    addOption(["7062150", "OO", "1753", 0, "Manual air conditioning", "other"]),
+    addOption([
+      "7062150",
+      "OO",
+      "139671",
+      0,
+      "Plus pack - A1 Sportback",
+      "packs"
+    ]),
+    addOption([
+      "7062151",
+      "RA",
+      "140070",
+      1,
+      "Space saving spare wheel",
+      "other"
+    ]),
+    addOption(["7062151", "RA", "140071", 0, "Tool kit and Jack", "other"]),
+    addOption(["7062153", "OO", "69372", 0, "Tyre repair kit", "other"]),
+    addOption([
+      "7062153",
+      "OO",
+      "140070",
+      0,
+      "Space saving spare wheel",
+      "other"
+    ]),
+    addOption(["7062154", "OO", "2427", 0, "Tool kit", "other"]),
+    addOption(["7062154", "OO", "140071", 0, "Tool kit and Jack", "other"]),
     addOption([
       "7062192",
       "NW",
       "125089",
       1,
-      "Comfort and sound pack - A1 Sportback"
+      "Comfort and sound pack - A1 Sportback",
+      "packs"
     ]),
     addOption([
       "7062192",
       "NW",
       "125089",
       1,
-      "Comfort and sound pack - A1 Sportback"
+      "Comfort and sound pack - A1 Sportback",
+      "packs"
     ]),
     addOption([
       "7062192",
       "NW",
       "125089",
       1,
-      "Comfort and sound pack - A1 Sportback"
+      "Comfort and sound pack - A1 Sportback",
+      "packs"
     ]),
     addOption([
       "7062192",
       "NW",
       "125089",
       1,
-      "Comfort and sound pack - A1 Sportback"
+      "Comfort and sound pack - A1 Sportback",
+      "packs"
     ]),
-    addOption(["7062192", "NW", "129808", 0, "6 passive loudspeakers"]),
-    addOption(["7062347", "RA", "140070", 0, "Space saving spare wheel"]),
-    addOption(["7062347", "RA", "140070", 0, "Space saving spare wheel"]),
-    addOption(["7062347", "RA", "140071", 1, "Tool kit and Jack"])
+    addOption([
+      "7062192",
+      "NW",
+      "129808",
+      0,
+      "6 passive loudspeakers",
+      "other"
+    ]),
+    addOption([
+      "7062347",
+      "RA",
+      "140070",
+      0,
+      "Space saving spare wheel",
+      "other"
+    ]),
+    addOption([
+      "7062347",
+      "RA",
+      "140070",
+      0,
+      "Space saving spare wheel",
+      "other"
+    ]),
+    addOption(["7062347", "RA", "140071", 1, "Tool kit and Jack", "other"])
   )({
     rules: {},
     options: {},
     selected: [],
-    activeRules: []
+    activeRules: [],
+    // A manual hardcoded linked list for now
+    categories: {
+      values: [
+        "colour",
+        "trim",
+        "exterior body features",
+        "wheels",
+        "packs",
+        "other"
+      ],
+      current: "colour",
+      position: 0,
+      size: 6,
+      hasPrevious: false,
+      hasNext: true
+    }
   });
+};
 
 // VIEWS
 const Option = ({ option, state }) => {
@@ -314,12 +546,25 @@ const Option = ({ option, state }) => {
 };
 app({
   init: getInitialState(),
-  view: state => (
-    <div>
-      {collection(state.options).map(option => (
-        <Option option={option} state={state} />
-      ))}
-    </div>
-  ),
+  view: state => {
+    console.log({ state });
+    return (
+      <div>
+        <ul>
+          {state.categories.values.map(name => (
+            <li>
+              {name === state.categories.current ? name.toUpperCase() : name}
+            </li>
+          ))}
+        </ul>
+        {collection(state.options)
+          .filter(({ category }) => category === state.categories.current)
+          .map(option => (
+            <Option option={option} state={state} />
+          ))}
+        <button onclick={NextCategory}>Next</button>
+      </div>
+    );
+  },
   node: document.getElementById("app")
 });
